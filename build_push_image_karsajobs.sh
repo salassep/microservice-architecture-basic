e15d1f7d18e9eaf64157ff7ad7b6f582e04e85e6
Salas Sepkardianto <66544116+salassep@users.noreply.github.com>
2022-12-28 16:38:09 +0800

    add explanation

--- build_push_image_karsajobs.sh
+++ build_push_image_karsajobs.sh
@@ -1,9 +1,13 @@
 #! /bin/sh
 
+# Create a docker image from a dockerfile with the name salassep/karsajobs
 docker build -t salassep/karsajobs:latest .
 
+# Adjust the docker image name according to the github packages format
 docker tag salassep/karsajobs:latest ghcr.io/salassep/karsajobs:latest
 
+# Login to github packages using token
 echo $CR_PAT | docker login ghcr.io -u salassep --password-stdin
 
+# Push the docker image to github packages
 docker push ghcr.io/salassep/karsajobs:latest
